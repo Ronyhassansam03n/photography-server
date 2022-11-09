45eab1ea7ed9ab6a88e2fa2193eddcc574c87f2b
Ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-09 19:01:20 +0600

    database done

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use (cors())
 app.use(express.json())
+
+app.get('/', (req, res)=>{
+
+    res.send('website server is running')
+
+})
+
+
+
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9pyp5ct.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`server is online on ${port}`)
+})
