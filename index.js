3c3c5266cc4ffe6fcaa09272e6f45f8283abad19
Ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-09 20:17:36 +0600

    mongo db done

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-app.use (cors())
+app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
 
     res.send('website server is running')
 
@@ -22,17 +22,32 @@ console.log(process.env.DB_PASSWORD)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9pyp5ct.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+
+    try {
+
+        const servicesCollection = client.db('photographer').collection('services')
+
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(error => console.error(error))
 
 
 
 
 
-
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`server is online on ${port}`)
 })
