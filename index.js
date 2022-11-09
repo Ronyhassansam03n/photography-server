17478ecb0a24a4232a7add1f533208f2e13603ee
Ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-09 22:21:43 +0600

    mongo db init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -34,6 +34,15 @@ async function run() {
             const cursor = servicesCollection.find(query)
             const services = await cursor.toArray();
             res.send(services)
+        })
+
+        app.get('/services/:id', async(req, res) =>{
+
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)}
+            const service = await  servicesCollection.findOne(query)
+            res.send(service);
+
         })
 
     }
