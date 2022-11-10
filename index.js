e39b28c468b803c671a6681998a9c881d6043013
Ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-10 22:20:19 +0600

    all done

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.get('/', (req, res) => {
 
 
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9pyp5ct.mongodb.net/?retryWrites=true&w=majority`;
 
